b86175595abea87e75362150d90cdfbe07cafa1b
Azad Mosarof <98151626+Azad-mosarof@users.noreply.github.com>
2023-07-02 09:34:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from deepface import DeepFace
-import cv2
+from io import BytesIO
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 import numpy as np
+from PIL import Image
 
 app = FastAPI()
 
@@ -32,11 +33,14 @@ def main():
 def verify(images: Images):
 
     response1 = requests.get(str(images.image1))
-    response4 = requests.get(str(images.image2))
+    response2 = requests.get(str(images.image2))
 
-    img1_array = cv2.imdecode(np.array(bytearray(response1.content), dtype=np.uint8), -1)
-    pic4_array = cv2.imdecode(np.array(bytearray(response4.content), dtype=np.uint8), -1)
+    img1 = Image.open(BytesIO(response1.content))
+    img2 = Image.open(BytesIO(response2.content))
 
-    result = DeepFace.verify(img1_array, pic4_array, model_name='VGG-Face')
+    img1_array = np.array(img1)
+    img2_array = np.array(img2)
+
+    result = DeepFace.verify(img1_array, img2_array, model_name='VGG-Face')
     result['verified'] = bool(result['verified'])
     return result
